74b513ddd4b697feab0a8d7b62096e89fb373e5d
Nail Valeev <nv222ch@student.lnu.se>
2018-12-21 15:19:25 +0100

    desktop js & css, create() method

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Nail Valeev
+ * @version 1.1.0
+ */
+
+'use strict'
+
+import Desktop from './desktop.js'
+
+var dt = new Desktop()
+
+dt.create()
