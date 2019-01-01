356fa9c11b1b8f2286767c19d3b3a9a0143381e4
NailValeev <nv222ch@student.lnu.se>
2019-01-01 21:19:08 +0100

    Game: gameover handling, lose case

--- src/js/Game.js
+++ src/js/Game.js
@@ -26,6 +26,7 @@ export default class Game extends PWDWindow {
     
     gameFrame.querySelector('#game-start-btn').addEventListener('click', (e) => {
       e.stopPropagation()
+      this.startBtn.style.display = 'none'
       this.init() 
     })
     
@@ -33,30 +34,52 @@ export default class Game extends PWDWindow {
     this.gameBody.classList.toggle('game-body')
     this.gameBoard = gameFrame.querySelector('.app-board')
     this.infoBlock = gameFrame.querySelector('.app-info')
+    this.startBtn = gameFrame.querySelector('#game-start-btn')
     holder.appendChild(gameFrame)
     
     this.expose()
+    
+    let thisGame = document.querySelector('#' + this.domId)
+    this.canvas = thisGame.querySelector('.game-canvas')
+    this.context = this.canvas.getContext('2d')
   }
   
   init () {
     console.log('init()')
-    let thisGame = document.querySelector('#' + this.domId)
-    this.canvas = thisGame.querySelector('.game-canvas')
-    this.context = this.canvas.getContext('2d')
+
+    if (this.gameOver){
+      while (this.gameBoard.childElementCount > 1) {
+        this.gameBoard.removeChild(this.gameBoard.firstChild) // To clear old results
+      }
+      this.gameOver = false
+      this.canvas.style.display = 'block'
+    }
+
     this.x = this.canvas.width/2
     this.y = this.canvas.height-30
     this.bookX = (this.canvas.width - this.bookWidth) / 2
-    setInterval( () => {this.draw()}, 20) // 50 fps
+    this.animation = setInterval( () => {this.draw()}, 20) // 50 fps
   }
   
   draw () {
-    console.log('this.deltaY ' + this.deltaY)
-    console.log('this.y ' + this.y)
-    if ( (this.y + this.deltaY > this.canvas.height-this.ballRadius) || (this.y + this.deltaY < this.ballRadius ) ) {
-      this.deltaY = -this.deltaY
-    }
     if ( (this.x + this.deltaX > this.canvas.width-this.ballRadius) || (this.x + this.deltaX < this.ballRadius ) ) {
       this.deltaX = -this.deltaX
+    }
+
+    if ( this.y + this.deltaY < this.ballRadius ) {
+      this.deltaY = -this.deltaY
+    }
+    else if ( this.y + this.deltaY > this.canvas.height-this.ballRadius) {
+      if(this.x > this.bookX && this.x < this.bookX + this.bookWidth) {
+        this.deltaY = -this.deltaY
+      } else {
+        this.gameOver = true
+        clearInterval(this.animation)
+        this.startBtn.style.display = 'inline'
+        console.log('You lose')
+        this.message('You LOSE !', false)
+        // TODO Implement UI for loser 
+      }
     }
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.drawBall()
@@ -86,5 +109,32 @@ export default class Game extends PWDWindow {
     this.context.fillStyle = "#0095DD"
     this.context.fill()
     this.context.closePath()
-}
+  }
+
+  message (message, winnerFlag) {
+    console.log('winner: ' + winnerFlag)
+
+    let board = document.createElement('div')
+    board.classList.toggle('memory-board')
+
+    this.canvas.style.display = 'none'
+
+    this.gameBoard.insertBefore(board, this.canvas)
+
+    let infoSpan = document.createElement('span')
+    infoSpan.classList.toggle('alert-msg')
+    infoSpan.innerHTML = message
+
+    let ImgDiv = document.createElement('div')
+    ImgDiv.classList.toggle('alert-img-holder')
+
+    if (winnerFlag) {
+      ImgDiv.classList.toggle('alert-winner-holder')
+    } else {
+      ImgDiv.classList.toggle('alert-loser-holder')
+    }
+
+    board.appendChild(infoSpan)
+    board.appendChild(ImgDiv)
+  }
 }
