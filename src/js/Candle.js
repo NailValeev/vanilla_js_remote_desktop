116cf7f366e33539e255ba4a266f74552d478e3f
NailValeev <nv222ch@student.lnu.se>
2019-01-02 14:21:44 +0100

    Candle.js JSDocs

--- src/js/Candle.js
+++ src/js/Candle.js
@@ -1,8 +1,20 @@
+/**
+* Class for candle light
+*
+* @author Nail Valeev
+* @version 1.1.0
+*/
 export default class Candle {
-
-    constructor (x, y, activityStatus){
-        this.x = x
-        this.y = y
-        this.active = activityStatus
-    }
+  /**
+  * Candle will be created with pre-defined coordinates and activity status
+  * @constructor
+  * @param {number} x x coordinate for the candle light
+  * @param {number} y y coordinate for the candle light
+  * @param {Boolean} activityStatus status to define display candle light or not
+  */
+  constructor (x, y, activityStatus){
+    this.x = x
+    this.y = y
+    this.active = activityStatus
+  }
 }
